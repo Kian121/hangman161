5cd6f18e2702357b145283984648e45faae89e43
Kian <149825448+Kian121@users.noreply.github.com>
2023-12-08 19:32:05 +0000

    Update Hangman_Game.py
    Inserted docstrings

--- Hangman_Game.py
+++ Hangman_Game.py
@@ -1,9 +1,10 @@
 import random
 
+
 class Hangman:
     """
-    A Hangman game class which represents a single game.
-    
+    A Hangman game class which represents a single game session.
+
     Attributes:
         word (str): The word to be guessed.
         word_guessed (list): List representing the characters guessed by the user.
@@ -14,11 +15,11 @@ class Hangman:
     
     def __init__(self, word_list, num_lives=5):
         """
-        The constructor for Hangman class.
-        
+        Initializes a new game of Hangman with a randomly selected word from the given list.
+
         Parameters:
-            word_list (list): The list of words from which the game chooses.
-            num_lives (int): The number of lives the player starts with (default is 5).
+            word_list (list of str): A list of words from which the game randomly chooses one.
+            num_lives (int): The number of incorrect guesses allowed before the game is lost (default is 5).
         """
         self.word = random.choice(word_list).lower()  # The mystery word
         self.word_guessed = ['_' for _ in self.word]  # List to track the guessed word
@@ -26,25 +27,31 @@ class Hangman:
         self.num_lives = num_lives  # Number of lives the player starts with
         self.list_letters = []  # List to keep track of guessed letters
 
-        # Prints the number of unique letters in the word
-        print(f"The mystery word has {self.num_letters} characters")
+        # Initial message to the player.
+        print(f"The mystery word has {self.num_letters} unique characters.")
         print(' '.join(self.word_guessed))
 
     def check_letter(self, letter):
         """
-        Check if the letter provided by the user is in the word and update game state accordingly.
-        
+        Processes the player's letter guess and updates the game's state accordingly.
+
+        If the letter is in the word and hasn't been guessed before, it reveals the letter's position(s) in the word.
+        If the letter is not in the word, it decrements the number of lives left.
+
         Parameters:
-            letter (str): The letter to check.
-            
+            letter (str): The letter guessed by the player.
+
         Returns:
             None
         """
         letter = letter.lower()
+        if letter not in self.list_letters:
+            self.list_letters.append(letter)
+
         if letter in self.word:
-            if letter not in self.list_letters:
+            if letter not in self.word_guessed:
                 self.num_letters -= 1
-                self.list_letters.append(letter)
+            
             for i, char in enumerate(self.word):
                 if char == letter:
                     self.word_guessed[i] = letter
@@ -54,28 +61,32 @@ class Hangman:
 
     def ask_letter(self):
         """
-        Prompt the player to input a letter and validate it.
-        
+        Requests a letter from the player and ensures it is a valid single alphabetical character.
+        Re-prompts the player until a valid letter is entered.
+
         Returns:
             None
         """
         while True:
             letter = input("Guess a letter: ")
             if len(letter) != 1 or not letter.isalpha():
-                print("Please, enter just one character")
+                print("Please, enter just one alphabetical character.")
             elif letter in self.list_letters:
-                print(f"{letter} was already tried")
+                print(f"You have already tried '{letter}'.")
             else:
                 self.check_letter(letter)
                 break
 
+
 def play_game(word_list):
     """
-    Function to initiate and control the flow of the Hangman game.
-    
+    Begins and manages a game of Hangman.
+
+    Continues to prompt for guesses until the player either wins (guesses all letters) or loses (runs out of lives).
+
     Parameters:
-        word_list (list): The list of words from which the game chooses.
-        
+        word_list (list of str): A list of words from which the game randomly chooses one to start a new game.
+
     Returns:
         None
     """
@@ -84,12 +95,14 @@ def play_game(word_list):
         if game.num_lives == 0:
             print(f"You lost! The word was '{game.word}'.")
             break
-        elif game.num_letters == 0:
+        elif '_' not in game.word_guessed:
             print("Congratulations! You won!")
+            print(f"The word was '{game.word}'.")
             break
         else:
             print(' '.join(game.word_guessed))
             game.ask_letter()
+
 
 if __name__ == '__main__':
     # List of words used for the Hangman game
